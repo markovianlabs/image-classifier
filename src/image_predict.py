ca72adce9f512ced83ea58c7ce9a094192e72afd
Jverma <j.verma5@gmail.com>
2017-02-06 11:51:10 +0530

    image predict based on pretrained vgg

---
+++ src/image_predict.py
@@ -0,0 +1,105 @@
+import os
+import h5py
+import cv2
+import sys
+import numpy as np
+
+
+from keras.preprocessing.image import ImageDataGenerator
+from keras import optimizers
+from keras.models import Sequential
+from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.optimizers import SGD
+
+from keras import backend as K
+K.set_image_dim_ordering('th')
+
+
+# path to the model weights files.
+weights_path = '../data/vgg16_weights.h5'
+
+# dimensions of our images.
+img_width, img_height = 224, 224
+
+
+
+# build the VGG16 network
+model = Sequential()
+model.add(ZeroPadding2D((1, 1), input_shape=(3, img_width, img_height)))
+
+model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_1'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_2'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_1'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(128, 3, 3, activation='relu', name='conv2_2'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_1'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_2'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(256, 3, 3, activation='relu', name='conv3_3'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_1'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_2'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(512, 3, 3, activation='relu', name='conv4_3'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_1'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_2'))
+model.add(ZeroPadding2D((1, 1)))
+model.add(Convolution2D(512, 3, 3, activation='relu', name='conv5_3'))
+model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(4096, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(4096, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1000, activation='softmax'))
+
+model.load_weights(weights_path)
+
+
+class_dict = {}
+index_file = open('synset_words.txt')
+for i,line in enumerate(index_file):
+	record = line.rstrip().split(' ')
+	class_dict[i] = record[1:]
+
+
+input_image = sys.argv[1]
+im = cv2.resize(cv2.imread(input_image), (224, 224)).astype(np.float32)
+im[:,:,0] -= 103.939
+im[:,:,1] -= 116.779
+im[:,:,2] -= 123.68
+im = im.transpose((2,0,1))
+im = np.expand_dims(im, axis=0)
+
+# Test pretrained model
+sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(optimizer=sgd, loss='categorical_crossentropy')
+out = model.predict_proba(im)
+
+# idx = np.argmax(out)
+# print class_dict[idx]
+
+idxs = np.argsort(out[0])[::-1][:5]
+for x in idxs:
+	print class_dict[x]
+
+
+
+
